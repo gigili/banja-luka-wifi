0f94bf71023b031e41d825e78b0b9f79421f3807
Igor Ilic <igorilicbl@gmail.com>
2020-06-30 20:34:22 +0200

    Start working on add/edit form

--- app/src/main/java/com/gac/banjalukawifi/db/entities/Network.kt
+++ app/src/main/java/com/gac/banjalukawifi/db/entities/Network.kt
@@ -5,9 +5,16 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "networks")
 class Network(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0, val name: String = "", val password: String = "", val address: String? = null,
+    val name: String = "", val password: String = "", val address: String? = null,
     val geoLat: String? = null, val geoLong: String? = null, val userID: String? = null
 ) {
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+    
+    fun setID(value : Int){
+        id = value
+    }
 
     override fun toString(): String {
         return "${this.name} \n${this.address}"
--- app/src/main/java/com/gac/banjalukawifi/ui/AddEditNetworkFragment.kt
+++ app/src/main/java/com/gac/banjalukawifi/ui/AddEditNetworkFragment.kt
@@ -1,11 +1,16 @@
 package com.gac.banjalukawifi.ui
 
+import android.os.AsyncTask
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.gac.banjalukawifi.R
+import com.gac.banjalukawifi.db.AppDatabase
+import com.gac.banjalukawifi.db.entities.Network
+import kotlinx.android.synthetic.main.fragment_add_edit_network.*
 
 class AddEditNetworkFragment : Fragment() {
 
@@ -14,11 +19,26 @@ class AddEditNetworkFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit_network, container, false)
-    }
+        val view =  inflater.inflate(R.layout.fragment_add_edit_network, container, false)
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = AddEditNetworkFragment()
+        val appDB = AppDatabase.getDatabase(requireContext().applicationContext)
+
+        val mBntSave = view.findViewById<Button>(R.id.btnSave)
+        mBntSave.setOnClickListener {
+            val network = Network(
+                edtNetworkName.text.toString(),
+                edtNetworkPassword.text.toString(),
+                edtNetworkAddress.text.toString(),
+                "0",
+                "0",
+                ""
+            )
+
+            AsyncTask.execute {
+                appDB.networkDao().insert(network)
+            }
+        }
+
+        return view
     }
 }
